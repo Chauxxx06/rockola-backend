b6fd2773168dfe6348908af4627b4507a9da0220
ccjaviers777 <javie.cardozo@gmail.com>
2023-07-28 15:47:05 -0500

    Version 1.0

---
+++ src/main/java/com/rockola/api/entity/TipoUsuario.java
@@ -0,0 +1,45 @@
+package com.rockola.api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tipo_usuario")
+public class TipoUsuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tipo_usuario")
+    private Integer idTipoUsuario;
+
+    @Column(name = "nombre_tipo_usuario")
+    private String nombreTipoUsuario;
+
+    @Column(name = "descripcion_tipo_usuario")
+    private String descripcionTipoUsuario;
+
+    public Integer getIdTipoUsuario() {
+        return idTipoUsuario;
+    }
+
+    public void setIdTipoUsuario(Integer idTipoUsuario) {
+        this.idTipoUsuario = idTipoUsuario;
+    }
+
+    public String getNombreTipoUsuario() {
+        return nombreTipoUsuario;
+    }
+
+    public void setNombreTipoUsuario(String nombreTipoUsuario) {
+        this.nombreTipoUsuario = nombreTipoUsuario;
+    }
+
+    public String getDescripcionTipoUsuario() {
+        return descripcionTipoUsuario;
+    }
+
+    public void setDescripcionTipoUsuario(String descripcionTipoUsuario) {
+        this.descripcionTipoUsuario = descripcionTipoUsuario;
+    }
+}
